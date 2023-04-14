037dbaec93247ac0f853af68217fbab9788462a4
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-04-14 17:51:31 +0530

    Add original and result images

--- app.py
+++ app.py
@@ -29,16 +29,29 @@ def index():
         # Reading the text from the image
         reader = easyocr.Reader(['en'])
         data = reader.readtext(np.array(img))
-
-        # For one line of text
-        # text = data[0][1]
+        print(data)  # For debugging
 
         # For multiple lines
         text = ""
         for i in range(len(data)):
             text += data[i][1] + " "
 
-        print(data)  # For debugging
+        # Putting rectangle around the text
+        result_image = np.array(img)
+        for detection in data:
+            top_left = tuple([int(val) for val in detection[0][0]])
+            bottom_right = tuple([int(val) for val in detection[0][2]])
+            text = detection[1]
+
+            result_image = cv2.rectangle(
+                result_image, top_left, bottom_right, (0, 255, 0), 5)
+            result_image = cv2.putText(result_image, text, top_left, cv2.FONT_HERSHEY_SIMPLEX,
+                                       2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        # Encode result_image as base64 string
+        _, result_image_buffer = cv2.imencode('.jpg', result_image)
+        result_image_base64 = base64.b64encode(
+            result_image_buffer).decode('utf-8')
 
         # Converting to speech
         language = 'en'
@@ -61,6 +74,7 @@ def index():
 
         # Result
         result = {'image': image_base64,
+                  'result_image': result_image_base64,
                   'text': text,
                   'speech': speech_base64
                   }

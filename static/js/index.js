c6da6fd25af22b3bdc9e172ad31d409eb5a55272
Hardik Pawar <hardikhpawar.cs21@rvce.edu.in>
2023-04-14 19:12:30 +0530

    Add file details section

--- static/js/index.js
+++ static/js/index.js
@@ -1,3 +1,14 @@
 function showLoadingSpinner() {
   document.getElementById("loading-spinner2").style.display = "block";
 }
+
+const inputFile = document.getElementById("imageFile");
+
+inputFile.addEventListener("change", function () {
+  const fileNameContainer = document.getElementById("file-name-container");
+
+  // Displaying the file name and size
+  fileNameContainer.textContent = `${this.files[0].name} (${(
+    this.files[0].size / 1000
+  ).toFixed(2)} KB)`;
+});
